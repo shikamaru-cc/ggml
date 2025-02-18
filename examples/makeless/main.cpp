6697ceff18f31ca6ed979b936f54cca90429ce99
shikamaru <shikamaru.cc@proton.me>
2025-02-18 18:22:21 +0800

    little batch work

--- examples/makeless/main.cpp
+++ examples/makeless/main.cpp
@@ -80,6 +80,7 @@ std::unique_ptr<encoder_t> encoder_load(const std::string & fname) {
 
     while (it != eos) {
         const auto ch = utf8::next(it, eos);
+        if (ch == '\n') continue;
         encoder->stoi[ch] = index++;
         encoder->itos.emplace_back(ch);
     }
@@ -135,73 +136,133 @@ int main(int argc, const char ** argv) {
         exit(0);
     }
 
+    const size_t n_batch  = 1000;
+    const size_t n_block  = 3;
+    const size_t n_vocab  = 2271;
+    const size_t n_embed  = 2;
+    const size_t n_hidden = 10000;
+    const size_t n_thread = 32;
+
     auto names = makeless_data_load(argv[1]);
     GGML_ASSERT(!names.empty());
 
     auto encoder = encoder_load(argv[2]);
     GGML_ASSERT(encoder);
+    GGML_ASSERT(encoder->stoi.size() == n_vocab);
+    GGML_ASSERT(encoder->itos.size() == n_vocab);
 
     std::vector<int32_t> tokens;
     std::transform(names.begin(), names.end(), std::back_inserter(tokens), [&](char32_t x){ return encoder->stoi[x]; });
 
-    // struct ggml_context * ctx;
-    // {
-    //     struct ggml_init_params params {
-    //         /*.mem_size   =*/ 1 << 30,
-    //         /*.mem_buffer =*/ NULL,
-    //         /*.no_alloc   =*/ false,
-    //     };
-    //     ctx = ggml_init(params);
-    // }
+    const size_t n_data = tokens.size() - n_block;
 
-    // size_t n_batch  = 1024;
-    // size_t n_block  = 3;
-    // size_t n_vocab  = 2271;
-    // size_t n_embed  = 2;
-    // size_t n_hidden = 10000;
-    // size_t n_thread = 32;
+    int32_t * data_input = new int32_t [n_block * n_data];
+    float   * data_label = new float   [n_vocab * n_data];
 
-    // ggml_tensor * data      = ggml_new_tensor_2d(ctx, GGML_TYPE_I32, n_block, tokens.size() / n_block);
+    memset(data_label, 0, n_vocab * n_data * sizeof(*data_label));
 
-    // ggml_tensor * input     = ggml_new_tensor_2d(ctx, GGML_TYPE_I32, n_block, n_batch);
-    // ggml_tensor * index     = ggml_view_1d(ctx, input, n_block * n_batch, 0);
+    // for (size_t i = 0; i + n_block < tokens.size(); ++i) {
+    for (size_t i = 0; i < n_batch; ++i) {
+        int32_t * ptr_input = data_input + i * n_block;
+        memcpy(ptr_input, tokens.data() + i, n_block * sizeof(*ptr_input));
 
-    // ggml_tensor * C         = ggml_new_tensor_2d(ctx, GGML_TYPE_F32, n_embed, n_vocab);
-    // ggml_tensor * emb       = ggml_get_rows(ctx, C, index);
-    // ggml_tensor * emb_view  = ggml_view_2d(ctx, emb, n_embed * n_block, n_batch, emb->nb[1], 0);
+        int32_t   idx_label = tokens[i + n_block];
+        float   * ptr_label = data_label + i * n_vocab;
 
-    // ggml_tensor * w1        = ggml_new_tensor_2d(ctx, GGML_TYPE_F32, n_embed * n_block, n_hidden);
-    // ggml_tensor * b1        = ggml_new_tensor_1d(ctx, GGML_TYPE_F32, n_hidden);
+        ptr_label[idx_label] = 1.0f;
+    }
 
-    // ggml_tensor * hpreact   = ggml_add(ctx, ggml_mul_mat(ctx, w1, emb_view), b1);
-    // ggml_tensor * h         = ggml_tanh(ctx, hpreact);
+    // printf("%d, %d, %d\n", tokens.size(), data_input.size(), data_label.size());
 
-    // ggml_tensor * w2        = ggml_new_tensor_2d(ctx, GGML_TYPE_F32, n_hidden, n_vocab);
-    // ggml_tensor * b2        = ggml_new_tensor_1d(ctx, GGML_TYPE_F32, n_vocab);
+    struct ggml_context * ctx;
+    {
+        struct ggml_init_params params {
+            /*.mem_size   =*/ 1 << 30,
+            /*.mem_buffer =*/ NULL,
+            /*.no_alloc   =*/ false,
+        };
+        ctx = ggml_init(params);
+    }
 
-    // ggml_set_param(ctx, C);
-    // ggml_set_param(ctx, w2);
-    // ggml_set_param(ctx, b2);
-    // ggml_set_param(ctx, w1);
-    // ggml_set_param(ctx, b1);
+    // ggml_tensor * data      = ggml_new_tensor_2d(ctx, GGML_TYPE_I32, n_block, n_batch);
+    // memcpy(data->data,  data_input, ggml_nbytes(data));
 
-    // tensor_init_random(C);
-    // tensor_init_random(w2);
-    // tensor_init_random(b2);
-    // tensor_init_random(w1);
-    // tensor_init_random(b1);
+    ggml_tensor * label     = ggml_new_tensor_2d(ctx, GGML_TYPE_F32, n_vocab, n_batch);
+    memcpy(label->data, data_label, ggml_nbytes(label));
 
-    // ggml_tensor * logits    = ggml_add(ctx, ggml_mul_mat(ctx, w2, hpreact), b2);
+    ggml_tensor * input     = ggml_new_tensor_2d(ctx, GGML_TYPE_I32, n_block, n_batch);
+    memcpy(input->data, data_input, ggml_nbytes(input));
 
-    // ggml_cgraph * gf = ggml_new_graph_custom(ctx, GGML_DEFAULT_GRAPH_SIZE, false);
+    ggml_tensor * index     = ggml_view_1d(ctx, input, n_block * n_batch, 0);
 
-    // ggml_build_forward_expand(gf, logits);
-    // ggml_build_backward_expand(ctx, ctx, gf, false);
+    ggml_tensor * C         = ggml_new_tensor_2d(ctx, GGML_TYPE_F32, n_embed, n_vocab);
+    ggml_tensor * emb       = ggml_get_rows(ctx, C, index);
+    ggml_tensor * emb_view  = ggml_view_2d(ctx, emb, n_embed * n_block, n_batch, emb->nb[1], 0);
 
-    // ggml_graph_reset(gf);
-    // ggml_graph_compute_with_ctx(ctx, gf, n_thread);
+    ggml_tensor * w1        = ggml_new_tensor_2d(ctx, GGML_TYPE_F32, n_embed * n_block, n_hidden);
+    ggml_tensor * b1        = ggml_new_tensor_1d(ctx, GGML_TYPE_F32, n_hidden);
 
-    // printf("epoch %d, loss %f\n", 0, ggml_get_f32_1d(loss, 0));
+    ggml_tensor * hpreact   = ggml_add(ctx, ggml_mul_mat(ctx, w1, emb_view), b1);
+    ggml_tensor * h         = ggml_relu(ctx, hpreact);
+
+    ggml_tensor * w2        = ggml_new_tensor_2d(ctx, GGML_TYPE_F32, n_hidden, n_vocab);
+    ggml_tensor * b2        = ggml_new_tensor_1d(ctx, GGML_TYPE_F32, n_vocab);
+
+    ggml_set_param(ctx, C);
+    ggml_set_param(ctx, w2);
+    ggml_set_param(ctx, b2);
+    ggml_set_param(ctx, w1);
+    ggml_set_param(ctx, b1);
+
+    tensor_init_random(C);
+    tensor_init_random(w2);
+    tensor_init_random(b2);
+    tensor_init_random(w1);
+    tensor_init_random(b1);
+
+    ggml_tensor * logits = ggml_add(ctx, ggml_mul_mat(ctx, w2, h), b2);
+
+    // forward
+    ggml_cgraph * gf = ggml_new_graph_custom(ctx, GGML_DEFAULT_GRAPH_SIZE, true);
+
+    ggml_build_forward_expand(gf, logits);
+
+    // forward + backward
+    ggml_cgraph * gf_grad = ggml_graph_dup(ctx, gf);
+
+    ggml_tensor * loss = ggml_cross_entropy_loss(ctx, logits, label);
+    ggml_set_loss(loss);
+
+    ggml_build_forward_expand(gf_grad, loss);
+    ggml_build_backward_expand(ctx, ctx, gf_grad, false);
+
+    // forward + backward + optimize
+    ggml_cgraph * gf_opt = ggml_graph_dup(ctx, gf_grad);
+
+    const float learning_rate = 0.1f;
+
+    for (int i = ggml_graph_n_nodes(gf)-1; i >= 0; --i) {
+        ggml_tensor * node = ggml_graph_node(gf_opt, i);
+        ggml_tensor * grad = ggml_graph_get_grad(gf_opt, node);
+
+        if (node->flags & GGML_TENSOR_FLAG_PARAM) {
+            ggml_tensor * learned = ggml_sub_inplace(ctx, node, ggml_scale_inplace(ctx, grad, learning_rate));
+            ggml_build_forward_expand(gf_opt, learned);
+        }
+    }
+
+    // train
+    printf("training start\n");
+
+    for (int i = 0; i < 10; ++i) {
+        ggml_graph_reset(gf_opt);
+        ggml_graph_compute_with_ctx(ctx, gf_opt, n_thread);
+
+        printf("epoch %d, loss %f\n", i, ggml_get_f32_1d(loss, 0));
+    }
+
+    delete data_input;
+    delete data_label;
 
     return 0;
 }
