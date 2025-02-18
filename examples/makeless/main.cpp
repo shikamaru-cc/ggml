9b82cb937a5d6248bd4a1562dea17e4ee185b79d
shikamaru <shikamaru.cc@proton.me>
2025-02-18 13:43:59 +0800

    model

--- examples/makeless/main.cpp
+++ examples/makeless/main.cpp
@@ -11,6 +11,7 @@
 #include <set>
 #include <map>
 #include <memory>
+#include <random>
 
 // Generate token index
 //
@@ -58,7 +59,7 @@
 
 
 struct encoder_t {
-    std::map<char32_t, unsigned> stoi;
+    std::map<char32_t, int32_t> stoi;
     std::vector<char32_t> itos;
 };
 
@@ -75,7 +76,7 @@ std::unique_ptr<encoder_t> encoder_load(const std::string & fname) {
     std::istreambuf_iterator<char> it = fin.rdbuf();
     std::istreambuf_iterator<char> eos;
 
-    size_t index = 0;
+    int32_t index = 0;
 
     while (it != eos) {
         const auto ch = utf8::next(it, eos);
@@ -106,8 +107,26 @@ std::u32string makeless_data_load(const std::string & fname) {
 }
 
 
-#define MAKELESS_BATCH_SIZE 1024
-#define MAKELESS_BLOCK_SIZE 3
+#define TENSOR_DEBUG_SHAPE(t) \
+    do { \
+        printf(#t ": ne0=%ld ne1=%ld ne2=%ld ne3=%ld\n", (t)->ne[0], (t)->ne[1], (t)->ne[2], (t)->ne[3]); \
+    } while (0)
+
+
+void tensor_init_random(ggml_tensor * t) {
+    static std::random_device rd{};
+    static std::mt19937 gen{rd()};
+    static std::normal_distribution<float> nd{0.0f, 1e-2f};
+
+    GGML_ASSERT(t->type == GGML_TYPE_F32);
+    const int64_t ne = ggml_nelements(t);
+    std::vector<float> tmp(ne);
+
+    for (int64_t i = 0; i < ne; ++i) {
+        tmp[i] = nd(gen);
+    }
+    memcpy(t->data, tmp.data(), ggml_nbytes(t));
+}
 
 
 int main(int argc, const char ** argv) {
@@ -116,29 +135,73 @@ int main(int argc, const char ** argv) {
         exit(0);
     }
 
-    // auto names = makeless_data_load(argv[1]);
-    // GGML_ASSERT(!names.empty());
+    auto names = makeless_data_load(argv[1]);
+    GGML_ASSERT(!names.empty());
 
-    // auto encoder = encoder_load(argv[2]);
-    // GGML_ASSERT(encoder);
+    auto encoder = encoder_load(argv[2]);
+    GGML_ASSERT(encoder);
 
-    // std::vector<unsigned> tokens;
-    // std::transform(names.begin(), names.end(), std::back_inserter(tokens), [&](char32_t x){ return encoder->stoi[x]; });
+    std::vector<int32_t> tokens;
+    std::transform(names.begin(), names.end(), std::back_inserter(tokens), [&](char32_t x){ return encoder->stoi[x]; });
 
-    struct ggml_context * ctx;
-    struct ggml_init_params params {
-        /*.mem_size   =*/ 1 << 30,
-        /*.mem_buffer =*/ NULL,
-        /*.no_alloc   =*/ false,
-    };
-    ctx = ggml_init(params);
+    // struct ggml_context * ctx;
+    // {
+    //     struct ggml_init_params params {
+    //         /*.mem_size   =*/ 1 << 30,
+    //         /*.mem_buffer =*/ NULL,
+    //         /*.no_alloc   =*/ false,
+    //     };
+    //     ctx = ggml_init(params);
+    // }
 
-    ggml_tensor * input = ggml_new_tensor_2d(ctx, GGML_TYPE_I32, MAKELESS_BLOCK_SIZE, MAKELESS_BATCH_SIZE);
-    ggml_tensor * index = ggml_view_1d(ctx, input, MAKELESS_BATCH_SIZE * MAKELESS_BLOCK_SIZE, 0);
-    ggml_tensor * C     = ggml_new_tensor_2d(ctx, GGML_TYPE_F32, 2, 2271);
-    ggml_tensor * emb   = ggml_get_rows(ctx, C, index);
+    // size_t n_batch  = 1024;
+    // size_t n_block  = 3;
+    // size_t n_vocab  = 2271;
+    // size_t n_embed  = 2;
+    // size_t n_hidden = 10000;
+    // size_t n_thread = 32;
 
-    printf("shape of emb ne0=%ld ne1=%ld ne2=%ld ne3=%ld\n", emb->ne[0], emb->ne[1], emb->ne[2], emb->ne[3]);
+    // ggml_tensor * data      = ggml_new_tensor_2d(ctx, GGML_TYPE_I32, n_block, tokens.size() / n_block);
+
+    // ggml_tensor * input     = ggml_new_tensor_2d(ctx, GGML_TYPE_I32, n_block, n_batch);
+    // ggml_tensor * index     = ggml_view_1d(ctx, input, n_block * n_batch, 0);
+
+    // ggml_tensor * C         = ggml_new_tensor_2d(ctx, GGML_TYPE_F32, n_embed, n_vocab);
+    // ggml_tensor * emb       = ggml_get_rows(ctx, C, index);
+    // ggml_tensor * emb_view  = ggml_view_2d(ctx, emb, n_embed * n_block, n_batch, emb->nb[1], 0);
+
+    // ggml_tensor * w1        = ggml_new_tensor_2d(ctx, GGML_TYPE_F32, n_embed * n_block, n_hidden);
+    // ggml_tensor * b1        = ggml_new_tensor_1d(ctx, GGML_TYPE_F32, n_hidden);
+
+    // ggml_tensor * hpreact   = ggml_add(ctx, ggml_mul_mat(ctx, w1, emb_view), b1);
+    // ggml_tensor * h         = ggml_tanh(ctx, hpreact);
+
+    // ggml_tensor * w2        = ggml_new_tensor_2d(ctx, GGML_TYPE_F32, n_hidden, n_vocab);
+    // ggml_tensor * b2        = ggml_new_tensor_1d(ctx, GGML_TYPE_F32, n_vocab);
+
+    // ggml_set_param(ctx, C);
+    // ggml_set_param(ctx, w2);
+    // ggml_set_param(ctx, b2);
+    // ggml_set_param(ctx, w1);
+    // ggml_set_param(ctx, b1);
+
+    // tensor_init_random(C);
+    // tensor_init_random(w2);
+    // tensor_init_random(b2);
+    // tensor_init_random(w1);
+    // tensor_init_random(b1);
+
+    // ggml_tensor * logits    = ggml_add(ctx, ggml_mul_mat(ctx, w2, hpreact), b2);
+
+    // ggml_cgraph * gf = ggml_new_graph_custom(ctx, GGML_DEFAULT_GRAPH_SIZE, false);
+
+    // ggml_build_forward_expand(gf, logits);
+    // ggml_build_backward_expand(ctx, ctx, gf, false);
+
+    // ggml_graph_reset(gf);
+    // ggml_graph_compute_with_ctx(ctx, gf, n_thread);
+
+    // printf("epoch %d, loss %f\n", 0, ggml_get_f32_1d(loss, 0));
 
     return 0;
 }
